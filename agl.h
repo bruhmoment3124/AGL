da92e762cc3bd4d66a5501c491394b4272fd4c52
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-03-25 22:27:18 -0400

    rectangle drawing
    added a function to draw a rectangle

--- agl.h
+++ agl.h
@@ -72,6 +72,35 @@ void dline(int x1, int y1, int x2, int y2, int hex)
 	glEnd();
 }
 
+/*draw a rectangle*/
+void drect(int x, int y, int w, int h, int hex)
+{
+	/*color values*/
+	float r, g, b;
+	r = hex >> 16;
+	g = (hex & 0x00ff00) >> 8;
+	b = (hex & 0x0000ff);
+	
+	r /= 255.0f;
+	g /= 255.0f;
+	b /= 255.0f;
+	
+	/*convert opengl float values to standard X and Y*/
+	float q = (float)x / (float)sw;
+  float p = (float)y / (float)sh;
+	float s = (float)w / (float)sw;
+	float d = (float)h / (float)sw;
+	
+	glBegin(GL_POLYGON);
+		glVertex2f(q, p);
+		glVertex2f(q + s, p);
+		glVertex2f(q + s, p - d);
+		glVertex2f(q, p - d);
+		glVertex2f(q + s, p - d);
+		glColor3f(r, g, b);
+	glEnd();
+}
+
 /******************
 /keyboard functions
 /*****************/
