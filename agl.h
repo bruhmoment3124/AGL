9c3fd3cfb61a267ee92b658c2e70b2c3db17865d
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-03-29 20:07:12 -0400

    circles
    circle drawing has been added

--- agl.h
+++ agl.h
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <GLFW/glfw3.h>
 
 GLFWwindow *window; /*create window object*/
@@ -101,6 +102,35 @@ void drect(float x, float y, float w, float h, int hex)
 		glVertex2f(x, y - h);
 		glVertex2f(x + w, y - h);
 		glColor3f(r, g, b);
+	glEnd();
+}
+
+/*draw a circle*/
+void dcirc(float x, float y, float rad, int hex)
+{
+	/*color values*/
+	float r, g, b;
+	r = hex >> 16;
+	g = (hex & 0x00ff00) >> 8;
+	b = (hex & 0x0000ff);
+	r /= 255.0f; /*red*/
+	g /= 255.0f; /*green*/
+	b /= 255.0f; /*blue*/
+
+	/*convert opengl float values to standard X and Y*/
+	x /= (float)sw;
+  y /= (float)sh;
+	rad /= (float)sw;
+	
+	float theta;
+	glBegin(GL_POLYGON);
+		int i;
+		for(i=0; i<360; i++)
+		{
+			theta = i * 3.142 / 180;
+			glVertex2f(x + rad * cos(theta), y + rad * sin(theta));
+			glColor3f(r, g, b);
+		}
 	glEnd();
 }
 
