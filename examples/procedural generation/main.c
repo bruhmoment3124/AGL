00d1fe43c6955b41a75e5978a9a71f498c38694f
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-04-10 01:02:02 -0400

    procedural generation example

---
+++ examples/procedural generation/main.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include "../../AGL.h"
+
+/*generate a random map*/
+void gen(int cycles, int size, int hex)
+{
+	srand(time(0)); /*random seed*/
+	int x = 0, y = 0;
+	
+	int i;
+	for(i=0; i<=cycles; i++)
+	{
+		int dir = rand() % 4; /*random direction*/
+		if(dir == 0) drect(x, y += size, size, size, hex); /*up*/
+		if(dir == 1) drect(x += size, y, size, size, hex); /*right*/
+		if(dir == 2) drect(x, y -= size, size, size, hex); /*down*/
+		if(dir == 3) drect(x -= size, y, size, size, hex); /*left*/
+	}
+}
+
+main()
+{
+  win("Hello, World!", 500, 500);
+	while(loop(0xFFFFFF)) /*set window color*/
+	{
+		gen(1000, 32, 0x000000);
+		end();
+	}
+}
