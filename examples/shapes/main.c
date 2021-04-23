c34307e0c17665ace3044120950e235744c4147d
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-04-23 14:34:40 -0400

    input
    you can now press w, a, s and d to light up the 'dpad' in the lower right

--- examples/shapes/main.c
+++ examples/shapes/main.c
@@ -1,17 +1,30 @@
 #include "../../AGL.h"
+#include "../../AGLKEY.h"
 
 main()
 {
-  agl_window("Hello, World!", 500, 500);
-	agl_begin(0x000000);
-		agl_rectangle(250, -100, 25, 50, 0xFF0000); /*up*/
-		agl_rectangle(150, -200, 50, 25, 0xFF0000); /*left*/
-		agl_rectangle(300, -200, 50, 25, 0xFF0000);	/*right*/
-		agl_rectangle(250, -250, 25, 50, 0xFF0000); /*down*/
+  GLFWwindow *win = agl_window("Hello, World!", 500, 500);
+	
+	int c1 = 0xFF0000, c2 = 0xFF0000, c3 = 0xFF0000, c4 = 0xFF0000;
+	while(!glfwWindowShouldClose(win))
+	{
+		agl_begin(0x000000);
+			agl_rectangle(250, -100, 25, 50, c1); /*up*/
+			agl_rectangle(150, -200, 50, 25, c2); /*left*/
+			agl_rectangle(300, -200, 50, 25, c3);	/*right*/
+			agl_rectangle(250, -250, 25, 50, c4); /*down*/
 		
-		agl_circle(-350, 350, 150, 150, 0x0000FF);
-		agl_rectangle(-500, 200, 150, 150, 0x00FF00);
-		agl_line(-500, -500, -200, -100, 0xFF0000);
-		agl_line(-500, -100, -200, -500, 0xFF0000);
-	agl_end();
+			agl_circle(-350, 350, 150, 150, 0x0000FF);
+			agl_rectangle(-500, 200, 150, 150, 0x00FF00);
+			agl_line(-500, -500, -200, -100, 0xFF0000);
+			agl_line(-500, -100, -200, -500, 0xFF0000);
+		glfwSwapBuffers(win);
+		glfwPollEvents();
+		
+		if(agl_keyhold(87)) c1 = 0x00FF00;
+		if(agl_keyhold(65)) c2 = 0x00FF00;
+		if(agl_keyhold(68)) c3 = 0x00FF00;
+		if(agl_keyhold(83)) c4 = 0x00FF00;
+	}
+	glfwTerminate();
 }
